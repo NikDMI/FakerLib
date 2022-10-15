625de1d866e8ae9226b68de54605b4c5e3e8c233
NikDMI <88460941+NikDMI@users.noreply.github.com>
2022-10-15 23:25:03 +0300

    Добавьте файлы проекта.

---
+++ Faker/IFaker.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace FakerLib.Faker
+{
+    public interface IFaker
+    {
+        //Creates DTO
+        public T Create<T>();
+
+        //Generates values according to type of var
+        internal T GenerateValue<T>();
+    }
+}
